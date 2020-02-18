ae071063dbd318907ecd209f06ea8fe223576a72
Guilherme Rosa <anoobs@tutamail.com>
2020-02-18 17:15:34 -0300

    Comentário adicionados no exercício 076.

--- ex076.py
+++ ex076.py
@@ -4,9 +4,16 @@ Mostrando:
 a) Listagem dos preços organizando-os de forma tabular.
 '''
 tupla = ('Pão', 2.00, 'Suco', 2.50, 'Cerveja Skol', 5.50, 'Chocolate', 4.90)
+# a) repetição genérica para funcionar para qualquer tupla
+# A variável auxiliar 'c' irá começar no índice zero, o início, e percorrerá até o comprimento da tupla,
+# isto é, percorrerá até o final da tupla, 'len(tupla)', com passo 2.
 for c in range(0, len(tupla), 2):
+    # Alinhamento da tupla com índice 'c' à esquerda com 20 caracteres de espaçamento,
+    # preenchidos com '.'. O 'end = ''', serve para juntar o próximo print com este.
     print(f'{tupla[c]:.<20}', end='')
-    if c == 0 or c == 8:
+    # Condição para caso o Índice 'c' esteja no começo ou no fim, o indice seja somado com 1
+    if c == 0 or c == len(tupla):
         print(f'R$ {tupla[c + 1]:3}')
+    # Caso contrário, o Índice 'c' será subtraído com 1
     else:
         print(f'R$ {tupla[c - 1]:3}')
