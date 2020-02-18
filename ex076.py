0340de92aae9635f4c8f17efc72a7ca76ed2ef54
Guilherme Rosa <anoobs@tutamail.com>
2020-02-18 13:17:57 -0300

    Início do exercício 076

---
+++ ex076.py
@@ -0,0 +1,6 @@
+'''
+Programa com tupla única que contém nome e preço dos produtos
+Mostrando:
+a) Listagem dos preços organizando-os de forma tabular.
+'''
+tupla = ('Pão', 2.00, 'Suco', 2.50, 'Cerveja Skol', 5.50, 'Chocolate', 4.90)
