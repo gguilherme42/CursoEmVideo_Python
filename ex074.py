844d7186dd2093f6ac12434c555e0a9651c7d2d2
Guilherme Rosa <anoobs@tutamail.com>
2020-02-18 13:14:24 -0300

    Realização do Exercício 074

---
+++ ex074.py
@@ -0,0 +1,32 @@
+'''
+Programa que gera 5 números aleatórios em uma tupla.
+Mostrando:
+a) A listagem dos números;
+b) O maior e o menor número.
+'''
+from random import randint
+# Variáveis simples receberam números aleatórios, dado que: tuplas são imutáveis
+n1 = randint(0, 100)
+n2 = randint(0, 100)
+n3 = randint(0, 100)
+n4 = randint(0, 100)
+n5 = randint(0, 100)
+tupla = (n1, n2, n3, n4, n5)
+# a) Números listados em ordem
+print(sorted(tupla))
+# b)
+# Declaração das variáveis simples maior e menor
+maior = menor = 0
+# Repetição para percorrer a tupla e indicar qual o maior e o menor número
+#  'ind' representa o índice da tupla e 'c' a variável auxiliar para percorrer a tupla e mostrar o objeto dela
+for ind, c in enumerate(tupla):
+    # Se o índice da lista for igual à 1, então:
+    if ind == 1:
+        maior = c
+        menor = c
+    else:
+        if c > maior:
+            maior = c
+        elif c < maior and c < menor:
+            menor = c
+print(f'O maior número é {maior} e o menor número é {menor}.')
---
+++ ex075.py
@@ -0,0 +1,38 @@
+'''
+Programa que leia 4 valores pelo teclado e guarde numa tupla
+Mostrando:
+a) Quantas vezes apareceu o valor 9;
+b)Em que posição foi digitado o primeiro valor 3;
+c) Quais foram os números pares.
+'''
+# Números inteiros a serem lidos:
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite outro número: '))
+n4 = int(input('Digite outro número: '))
+tupla = (n1, n2, n3, n4)
+print(f'TUPLA: \n{tupla}')
+# a)
+print(f'O número 9 apareceu {tupla.count(9)} vez(es).')
+# b)
+# Teste para verificar se o valor 3 está na tupla
+if 3 in tupla:
+    print(f'O primeiro valor  3 foi digitado no índice {tupla.index(3)} da tupla.')
+else:
+    print('O valor 3 não foi digitado.')
+# c)
+# Declaração de variável simples para verificar se há algum número par
+tempar = 0
+# Repetição para percorrer a tupla e verificar se existe número(s) par.
+for c in tupla:
+    if c % 2 == 0:
+        tempar += 1
+# Se há números par, então:
+if tempar != 0:
+    print('Número(s) par:')
+    for ind, c in enumerate(tupla):
+        if c % 2 == 0:
+            print(c, end=' ')
+# Se não há, logo:
+else:
+    print('Não há número par.')
