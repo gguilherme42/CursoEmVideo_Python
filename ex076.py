e70b56898974c3a449ee37ee2016641391cd8ae1
Guilherme Rosa <anoobs@tutamail.com>
2020-02-18 17:06:33 -0300

    Realização do exercício 076

--- ex076.py
+++ ex076.py
@@ -4,3 +4,9 @@ Mostrando:
 a) Listagem dos preços organizando-os de forma tabular.
 '''
 tupla = ('Pão', 2.00, 'Suco', 2.50, 'Cerveja Skol', 5.50, 'Chocolate', 4.90)
+for c in range(0, len(tupla), 2):
+    print(f'{tupla[c]:.<20}', end='')
+    if c == 0 or c == 8:
+        print(f'R$ {tupla[c + 1]:3}')
+    else:
+        print(f'R$ {tupla[c - 1]:3}')
