89f82f064b2259e5267facb001d200273f0fdb97
Guilherme Rosa <anoobs@tutamail.com>
2020-02-19 14:58:47 -0300

    Exercício 077 com a resuloção do Guanabara.

--- ex077.py
+++ ex077.py
@@ -1,2 +1,12 @@
-while True:
-    tupla = (int(input('Digite: ')), int(input('Digite: ')))
+'''
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são suas vogais.
+
+Resulução do @Guanabara resumida
+'''
+tupla = ('pao', 'mochila', 'roupa')
+for palavra in tupla:
+    print(f'\nNa palavras {palavra.upper()} temos: ', end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(f'{letra}', end=' ')
