170f566d05326e647867596e9eb782015169fc17
Guilherme Rosa <anoobs@tutamail.com>
2020-02-22 02:51:03 -0300

    Realização do exercício 082.

--- ex082.py
+++ ex082.py
@@ -5,3 +5,32 @@ No final, mostre:
 a) O conteúdo das listas.
 '''
 #@Guilherme
+lista = []
+par = []
+impar = []
+while True:
+    num = int(input('Digite um número: '))
+    lista.append(num)
+    if num % 2 == 0:
+        par.append(num)
+    else:
+        impar.append(num)
+    # Pergunta para continuação do programa
+    while True:
+        perg = str(input('Quer continuar? [S/N] ')).strip()[0].upper()
+        # Consequências da resposta
+        if perg in 'NnSs':
+            break
+        else:
+            print('Resposta inválida!')
+    if perg in 'Nn':
+        break
+print(f'''
+Conteúdo das listas:
+Lista principal:
+{lista}
+Lista de números pares:
+{par}
+Lista de números ímpares:
+{impar}
+''')
