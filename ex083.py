d48b94065cbd01bb05c0f7f376ca9c1da2e0cb9c
Guilherme Rosa <anoobs@tutamail.com>
2020-02-22 15:39:16 -0300

    Início do exercício 083.

--- ex083.py
+++ ex083.py
@@ -4,3 +4,23 @@ O aplicativo deverá analisar se a expressão passada está com os parênteses a
  na ordem correta.
 '''
 #@Guilherme
+expres = input('Digite uma expressão matemática: ').strip()
+lista = []
+soma = divi = mult = sub = 0
+for l in expres:
+    if l not in ' ':
+        lista.append(l)
+    if l == '+':
+        soma = 1
+    elif l == '*':
+        mult = 1
+    elif l == '-':
+        sub = 0
+    elif l == '/':
+        divi = 1
+print(lista)
+if lista in '()':
+    if lista.count('(') == lista.count(')'):
+
+    else:
+        print('Expressão errada!')
