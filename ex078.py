f5016e2d221399cdbc765ece13b03ec952225b8f
gguilherme42 <anoobs@tutamail.com>
2020-02-28 13:42:26 -0300

    Resulução do Guanabara comentada no exercício 078.

--- ex078.py
+++ ex078.py
@@ -16,3 +16,28 @@ print(lista)
 print(f'Maior valor: {max(lista)}, posição: {lista.index(max(lista))}')
 print(f'Menor valor: {min(lista)}, posição: {lista.index(min(lista))}')
 
+'''
+# Resolução do Guanabara:
+lista = []
+mai = men = 0
+for c in range(0, 5):
+    lista.append(int(input(f'Digite um valor para a posição {c}:')))
+    if c == 0:
+        mai = men = lista[c]
+    else:
+        if lista[c] > mai:
+            mai = lista[c]
+        if lista[c] < men:
+            men = lista[c]
+print(f'Você digitou os valores {lista}')
+print(f'O maior valor digitado foi {mai} na posição ', end='')
+for i, v in enumerate(lista):
+    if v == mai;
+            print(f'{i}.... ', end='')
+print()
+print('O menor valor digitado foi {men} na posição ', end='')
+for i, v in enumerate(lista):
+    if v == mai;
+            print(f'{i}.... ', end='')
+print()
+'''
--- ex083.py
+++ ex083.py
@@ -6,25 +6,3 @@ O aplicativo deverá analisar se a expressão passada está com os parênteses a
 #@Guilherme
 expres = input('Digite uma expressão matemática: ').strip()
 lista = []
-soma = divi = mult = sub = 0
-for l in expres:
-    if l not in ' ':
-        lista.append(l)
-    if l == '+':
-        soma = 1
-    elif l == '*':
-        mult = 1
-    elif l == '-':
-        sub = 0
-    elif l == '/':
-        divi = 1
-print(lista)
-if '012345678910' in lista and '+-/*' in lista:
-        if '()' in lista:
-            if lista.count('(') == lista.count(')'):
-                if (lista.index(0, '(') and lista.index(-1, ')')) or lista.index(0, ')'):
-                    print('Expressão inválida!')
-            else:
-                print('Expressão inválida!')
-else:
-    print('Expressão inválida!')
