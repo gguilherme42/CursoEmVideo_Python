363728e369badb7ec95f6c0c923cb456ca0f5a2e
gguilherme42 <anoobs@tutamail.com>
2020-03-04 18:52:54 -0300

    Adição do exercício 086, matrizes.

--- ex086.py
+++ ex086.py
@@ -1,4 +1,7 @@
 '''
-Exercício 084
+Exercício 086
 
+Crie um programa que crie uma matriz de dimensão 3x3 e preencha
+com valores lidos pelo teclado. No final, mostre a matriz na tela,
+com a formatação correta.
 '''
