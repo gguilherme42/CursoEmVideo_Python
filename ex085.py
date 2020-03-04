4bca68a043f657da4b4fb423f58bdc4402cf7a84
gguilherme42 <anoobs@tutamail.com>
2020-03-04 18:48:26 -0300

    Adição do exercício 085.

--- ex085.py
+++ ex085.py
@@ -1,4 +1,8 @@
 '''
-Exercício 084
+Exercício 085
+
+Crie um programa onde o usuário possa digitar sete valores numéricos e
+cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
+No final, mostre os valores pares e ímpares em ordem crescente.
 
 '''
