650656c5ee70fd8a0ec40f1318aa3ce2228790c7
gguilherme42 <anoobs@tutamail.com>
2020-03-04 19:03:30 -0300

    Adição do exercício 089.

--- ex089.py
+++ ex089.py
@@ -1,4 +1,10 @@
 '''
-Exercício 084
+Exercício 089
 
+Crie um programa que leia o nome e duas notas de vários alunos
+e guarde tudo em uma lista composta. No final, mostre um boletim
+contendo a média de cada um e permita que o usuário possa mostras as
+notas de cada aluno individualmente.
+
+Obs: Três níveis dentro de uma lista.
 '''
