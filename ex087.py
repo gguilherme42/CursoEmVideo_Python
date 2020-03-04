61ff754bcf48cb7b45a5d1937dacca8af5ecb8cd
gguilherme42 <anoobs@tutamail.com>
2020-03-04 18:54:48 -0300

    Adição do exercício 087, matrizes.

--- ex087.py
+++ ex087.py
@@ -1,4 +1,8 @@
 '''
-Exercício 084
+Exercício 087
 
+Aprimore o desafio anterior (ex 086), mostrando no final:
+a) A soma de todos os valores digitados;
+b) A soma de todos os valores da terceira coluna:
+c) O maior valor da segunda linha.
 '''
