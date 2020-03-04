fbf0242178d2af4276182ba3850b2fd3cdc49920
gguilherme42 <anoobs@tutamail.com>
2020-03-04 18:45:54 -0300

    Adição do exercício 084.

---
+++ ex084.py
@@ -0,0 +1,12 @@
+'''
+Exercício 084
+
+Faça um programa que leia o nome e peso de várias pessoas, guardando tudo em
+uma lista. No final, mostre:
+a) Quantas pessoas foram cadastradas;
+b) Uma listagem com as pessoas pesadas;
+c) Uma listagem com as pessoas leves.
+
+pesado: 100kg
+leve: 60kg
+'''
