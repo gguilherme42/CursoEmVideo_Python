8f1bbc608e7b2104a5ca7d32510b44b39f0de116
gguilherme42 <anoobs@tutamail.com>
2020-03-04 18:58:05 -0300

    Adição do exercício 088.

--- ex088.py
+++ ex088.py
@@ -1,4 +1,8 @@
 '''
-Exercício 084
+Exercício 088
 
+Faça um programa que ajude um jogador da MEGA SENA a criar
+palpites. O programa vai perguntar quantos jogos serão
+gerados e vai sortear 6 números entre 1 e 60 para cada jogo,
+cadastrando tudo em uma lista composta.
 '''
