6e172e53be9cd337f8ef58e380c87627418fee1f
gguilherme42 <anoobs@tutamail.com>
2020-03-05 09:45:43 -0300

    Resolução do exercício 085.

--- ex085.py
+++ ex085.py
@@ -6,3 +6,21 @@ cadastre-os em uma lista única que mantenha separados os valores pares e ímpar
 No final, mostre os valores pares e ímpares em ordem crescente.
 
 '''
+par = []
+ímpar = []
+# Repetição para digitar sete números
+for x in range(0, 7):
+    n = int(input('Digite um número: '))
+    # Se o número do resto da divisão por 2 for igual à zero,
+    # então é par
+    if n % 2 == 0:
+        par.append(n)
+    # Se não, ímpar
+    else:
+        ímpar.append(n)
+# Copiando os valores das listas dentro de outra lista
+números = [[par[:]], [ímpar[:]]]
+print('-'*30)
+print(f'Números: {números}')
+print(f'Pares: {números[0]}')
+print(f'Ímpares: {números[1]}')
