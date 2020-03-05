e0eca26c19ae81d97289dd45da15c0db24bd2b24
gguilherme42 <anoobs@tutamail.com>
2020-03-05 11:22:01 -0300

    Adição de validação e comentários ao exercício 088.

--- ex088.py
+++ ex088.py
@@ -9,10 +9,19 @@ cadastrando tudo em uma lista composta.
 from random import randint
 megasena = []
 jogos = int(input('Quantos jogos serão gerados? '))
-for x in range(0, jogos):
-    palpite = [randint(1, 60), randint(1, 60), randint(1, 60),
-               randint(1, 60), randint(1, 60), randint(1, 60)]
-    megasena.append(palpite[:])
-    palpite.clear()
-for ind, c in enumerate(megasena):
-    print(f'Palpile {ind + 1}: \n{c}')
+# Validação da respota, pois esta deve ser maior que zero.
+if jogos != 0 and not(jogos < 0):
+    # Repetição de palpites para quantidades de jogos
+    for x in range(0, jogos):
+        # Geradção dos 6 números de 1 até 6 na lista
+        palpite = [randint(1, 60), randint(1, 60), randint(1, 60),
+                   randint(1, 60), randint(1, 60), randint(1, 60)]
+        # Adição do palpite em outra lista
+        megasena.append(palpite[:])
+        # Limpando o palpite passado
+        palpite.clear()
+    # Usa-se de um índice para dizer o palpite de cada jogo
+    for ind, c in enumerate(megasena):
+        print(f'Palpite para o jogo {ind + 1}: \n{c}')
+else:
+    print('Não pode-se gerar palpites para zero jogos ou menos que zero jogos ')
