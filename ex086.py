a3d6b33e2451154cd3196a85cd0e2e8099962af3
gguilherme42 <anoobs@tutamail.com>
2020-03-05 10:52:12 -0300

    Comentários adicionados ao exercício 086.

--- ex086.py
+++ ex086.py
@@ -7,14 +7,19 @@ com a formatação correta.
 '''
 matriz = []
 dados = []
+# Repetição dentro de repetição, pois uma matriz 3x3 tem:
+# 3 linhas e 3 colunas, assim: 9 elemtentos e duas repetições, aninhadas.
 for l in range(0, 3):
     for c in range(0, 3):
         dados.append(int(input('Digite um número: ')))
     matriz.append(dados[:])
+    # Varre-se a lista 'dados' porque necessita de novos elementos, sem os anteriores
+    # para a nova linha da matriz
     dados.clear()
 print('-' * 30)
 print(f'======{" MATRIZ 3 X 3 ":^4}=====')
 for x in matriz:
+    # 'x' vira uma lista, portando, deve-se percorre-lô
     for ind, n in enumerate(x):
         print(f'( {n:^4} ) ', end='')
         if ind == 2:
