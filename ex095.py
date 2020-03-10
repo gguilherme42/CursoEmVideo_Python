3b2a40cdb30daa6d75fe8a9f6478ef25462b3885
gguilherme42 <anoobs@tutamail.com>
2020-03-10 17:26:50 -0300

    Adição de validações.

--- ex095.py
+++ ex095.py
@@ -7,19 +7,35 @@ jogador.
 futebol = {}
 jogadores = []
 gol = []
-totg = 0
 while True:
     futebol['Nome'] = str(input('Nome do jogador(a): '))
-    futebol['Partidas'] = int(input('Partidas jogadas: '))
+    # Validação das partidas.
+    while True:
+        futebol['Partidas'] = int(input('Partidas jogadas: '))
+        if futebol['Partidas'] <= 0:
+            print('Resposta inválida!')
+        else:
+            break
     for c in range(0, futebol['Partidas']):
-        gol.append(int(input(f'Gols na {c + 1}ª partida: ')))
-        totg += gol[c]
+        # Validação dos gols
+        while True:
+            g = int(input(f'Gols na {c + 1}ª partida: '))
+            if g < 0:
+                print('Resposta inválida!')
+            else:
+                break
+        gol.append(g)
     futebol['Gols'] = gol[:]
-    futebol['Total de Gols'] = totg
+    futebol['Total de Gols'] = sum(gol)
     jogadores.append(futebol.copy())
     gol.clear()
-    totg = 0
-    perg = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    # Validação da pergunta
+    while True:
+        perg = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+        if perg in 'SN':
+            break
+        else:
+            print('Resposta inválida!')
     if perg in 'N':
         break
 print('=' * 35)
@@ -31,6 +47,7 @@ print('=' * 35)
 while True:
     perg1 = str(input('Quer ver o aproveitamento de cada jogador? [S/N] ')).strip().upper()[0]
     if perg1 in 'S':
+        # Verificação do número do jogador
         while True:
             jg = int(input('Qual jogador? (999 para sair)'))
             if jg <= 0 or (jg - 1) > len(jogadores):
@@ -44,5 +61,7 @@ while True:
                 print(f'{"Nº":<10}{"NOME":<10}{"GOLS":>10}')
                 print(f'{jg:<10}{jogadores[jg - 1]["Nome"]}   {jogadores[jg - 1]["Gols"]}')
 
-    if perg1 in 'N':
+    elif perg1 in 'N':
         break
+    else:
+        print('Resposta inválida!')
