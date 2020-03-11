cbac6567e6c41e8847a4ba2a8c7f18dcf787ad13
gguilherme42 <anoobs@tutamail.com>
2020-03-10 21:44:51 -0300

    Resolução do exercício 100.

--- ex100.py
+++ ex100.py
@@ -5,3 +5,26 @@ somaPar(). A primeira função vai sortear 5 números e vai colocá-los
 dentro da lista e a segunda função vai mostrar a soma entre todos os valores
 PARES sorteador pela função anterior.
 '''
+from random import randint
+
+# Função sorteia retorna valor, neste caso, uma lista com novos valores.
+def sorteia(l):
+    # Repetição genérica para sortear valor em qualquer lista
+    for i, v in enumerate(l):
+        v = randint(0, 100)
+    return l
+
+
+def somaPar(x):
+    soma = 0
+    for p in x:
+        if p % 2 == 0:
+            soma += p
+    print(f'A soma dos pares é: {soma}')
+
+
+lista = [randint(0, 100), randint(0, 100), randint(0, 100),
+         randint(0, 100), randint(0, 100)]
+print(lista)
+print(f'A lista sorteda é:\n{sorteia(lista)}')
+somaPar(lista)
