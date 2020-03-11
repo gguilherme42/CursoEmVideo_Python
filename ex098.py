1ec050f08ab678d197c95f48e1e710abbe685ec7
gguilherme42 <anoobs@tutamail.com>
2020-03-10 21:19:51 -0300

    Resolução do exercício 098.

--- ex098.py
+++ ex098.py
@@ -10,4 +10,34 @@ c) uma contagem personalizada (usuário digita os parâmetros).
 Obs: passo = 0, vira 1
 passo = negativo, vira positivo
 '''
+def esc():
+    print()
+    print('='*30)
 
+
+def contador(i, f, p):
+   if p == 0:
+       p = 1
+   if i > f:
+       for l in range(i, f - p, - p):
+            print(l, end=' ')
+   else:
+       for l in range(i, f + p, p):
+            print(l, end=' ')
+
+esc()
+# a)
+print('Contagem de 1 até 10, com passo 1: ')
+contador(1, 10, 1)
+esc()
+# b)
+print('Contagem de 10 até 0, com passo 2: ')
+contador(10, 0, 2)
+esc()
+# c)
+print('Contagem personalizada: ')
+n1 = int(input('Digite o início: '))
+n2 = int(input('Digite o fim: '))
+n3 = int(input('Digite o passo: '))
+contador(n1, n2, n3)
+esc()
