4aa15f78a12c05284d5608a78ef1268cfa9a0741
gguilherme42 <anoobs@tutamail.com>
2020-03-11 09:41:49 -0300

    Resolução do exercício 102.

--- ex102.py
+++ ex102.py
@@ -7,3 +7,25 @@ será mostrado ou não na tela o processo de cálculo fatorial.
 
 obs: função deve ser documentada
 '''
+
+
+def fatorial(num=1, show=False):
+    f = 1
+    if show == True:
+        for c in range(num, 0, - 1):
+            print(f'{c} ', end='')
+            f *= c
+        return print(f'= {f}')
+    else:
+        for c in range(num, 0, - 1):
+            print(f'{c} ', end='')
+            f *= c
+        return f
+
+
+n = int(input('Fatorial de qual número: '))
+p = str(input('Quer ver o processo? [S/N] ')).strip().upper()[0]
+if p in 'S':
+    fatorial(n, show=True)
+else:
+    fatorial(n)
