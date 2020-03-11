1f3ae889e30e21968277da54fcc15448f4f25838
gguilherme42 <anoobs@tutamail.com>
2020-03-10 21:29:22 -0300

    Resolução do exercício 099.

--- ex099.py
+++ ex099.py
@@ -4,3 +4,28 @@ maior(), que receb vários parâmetros com valores inteiros.
 Seu programa tem que analizar todos os valores e dizer qual deles
 é o maior.
 '''
+# Função com um único parâmetro, pois será passado uma lista como parâmetro.
+def maior(num):
+    mai = 0
+    for i, n in enumerate(num):
+        if i == 0:
+            mai = n
+        elif num[i] > mai:
+            mai = num[i]
+    print(f'O maior número é: {mai}')
+
+lista = []
+while True:
+    d = int(input('Digite um número: '))
+    lista.append(d)
+    # Validação da pergunta
+    while True:
+        p = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+        if p in 'SN':
+            break
+        else:
+            print('Resposta inválida!')
+    if p in 'N':
+        break
+# Colocando na tela o maior número da lista
+maior(lista)
