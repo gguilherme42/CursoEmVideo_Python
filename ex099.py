d5390ec49136134914088f497fbcbd4a302af04f
gguilherme42 <anoobs@tutamail.com>
2020-03-10 22:55:13 -0300

    Melhoria e adição de comentários.

--- ex099.py
+++ ex099.py
@@ -4,28 +4,41 @@ maior(), que receb vários parâmetros com valores inteiros.
 Seu programa tem que analizar todos os valores e dizer qual deles
 é o maior.
 '''
+from random import randint
+from time import sleep
+
+
 # Função com um único parâmetro, pois será passado uma lista como parâmetro.
 def maior(num):
     mai = 0
-    for i, n in enumerate(num):
-        if i == 0:
-            mai = n
-        elif num[i] > mai:
-            mai = num[i]
-    print(f'O maior número é: {mai}')
+    print('-'*30)
+    # Se a lista tiver comprimento, então:
+    if len(num) != 0:
+        for i, n in enumerate(num):
+            if i == 0:
+                mai = n
+            elif num[i] > mai:
+                mai = num[i]
+        print('Analisando valores...')
+        sleep(0.5)
+        for c in num:
+            print(f'{c}', end=' ', flush=True)
+            sleep(0.5)
+        print()
+        print(f'Total de números: {len(num)}')
+        print(f'O maior número é: {mai}')
+    else:
+        print('Analisando valores...')
+        sleep(0.5)
+        print('Lista vazia')
+    print('-'*30)
 
-lista = []
-while True:
-    d = int(input('Digite um número: '))
-    lista.append(d)
-    # Validação da pergunta
-    while True:
-        p = str(input('Quer continuar? [S/N]')).strip().upper()[0]
-        if p in 'SN':
-            break
-        else:
-            print('Resposta inválida!')
-    if p in 'N':
-        break
-# Colocando na tela o maior número da lista
-maior(lista)
+
+for c in range(0, 4):
+    # Cria uma lista vazia, com comprimento gerado pelo randint
+    lista = list(range(randint(0, 10)))
+    # Repetição que percorre a lsita genéria
+    for i, v in enumerate(lista):
+        lista[i] = randint(0, 100)
+    # Colocando na tela o maior número da lista para 4 listas diferentes
+    maior(lista)
