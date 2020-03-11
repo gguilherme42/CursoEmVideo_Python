04ab64f6883801155f8aa3904449c4a42af89282
gguilherme42 <anoobs@tutamail.com>
2020-03-11 11:13:36 -0300

    Realização do exercício 106.

--- ex106.py
+++ ex106.py
@@ -7,7 +7,34 @@ se encerrará.
 
 Obs: Use cores.
 '''
+dicio = {'fAzul': '\033[1;30;44m', 'fAma': '\033[1;30;43m',
+        'fBran': '\033[1;7;30m', 'fP': '\033[m'}
+
+def sys(f):
+    global dicio
+    c = len(f)
+    print(end=f'{dicio["fP"]}')
+    print(f'{dicio["fAma"]}~' * c)
+    print(f'{f}')
+    print('~' * c)
+    print(end=f'{dicio["fP"]}')
 
 def minis(n):
+    global dicio
+    fra = f'Acessando o manual do comeando {n}'
+    t = len(fra)
+    print(f'{dicio["fAzul"]}~' * t)
+    print(f'{fra}')
+    print('~' * t)
+    print(f'{dicio["fP"]}', end='')
+    print(f'{dicio["fBran"]}')
+    print(f'{help(n)}')
 
-    help(n)
+
+while True:
+    sys('   SISTEMA DE AJUDA PyHELP   ')
+    sis = str(input('Função ou Biblioteca >'))
+    if sis in 'FIM':
+        break
+    else:
+        minis(sis)
