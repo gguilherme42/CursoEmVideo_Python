d71fa49cbb4f6b719403e0967988c1818d588f10
gguilherme42 <anoobs@tutamail.com>
2020-03-12 20:58:16 -0300

    Criação de diretório e módulo exercício 107 e sua resolução.

---
+++ ex107/moeda.py
@@ -0,0 +1,28 @@
+'''
+Exercício 107:
+    Crie um módulo chamado moeda.py que tenha as funções aumentar(),
+    diminuir(), dobro() e metade(). Faça também um programa que importe
+    este módulo e algumas funções.
+'''
+
+
+def aumentar(n, a):
+    c = (n * a) / 100
+    l = c + n
+    return f'R${l:.2f}'
+
+
+def diminuir(n, a):
+    c = (n * a) / 100
+    l = n - c
+    return f'R${l:.2f}'
+
+
+def dobro(n):
+    d = n * 2
+    return f'R${d:.2f}'
+
+
+def metade(n):
+    m = n / 2
+    return f'R${m:.2f}'
---
+++ ex107/test.py
@@ -0,0 +1,6 @@
+from ex107 import moeda
+n = int(input('Digite um preço: R$'))
+print(f'{n} com aumento de 10% é igual a {moeda.aumentar(n, 10)}')
+print(f'{n} menos 10% é igual a {moeda.diminuir(n, 10)}')
+print(f'O dobro de {n} é igual a {moeda.dobro(n)}')
+print(f'A metade de {n} é igual a {moeda.metade(n)}')
